54b42ae9e519b696d9587d8176ab413339303ac2
Rafael Augusto da Silva Ribeiro <rafael@exla.com.br>
2014-10-02 10:37:45 -0300

    Testando

---
+++ js/init.js
@@ -0,0 +1,19 @@
+/**
+ * Created by rafae_000 on 02/10/2014.
+ */
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '361738637322502',
+        xfbml      : true,
+        version    : 'v2.1'
+    });
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/pt_BR/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
