206938bfb5237f42ffca015e8020ba1d2e0d3ab1
Rafael Augusto da Silva Ribeiro <rafael@exla.com.br>
2014-10-06 08:14:13 -0300

    Testando

--- js/init.js
+++ js/init.js
@@ -50,7 +50,7 @@ function adicionarUserInRoles(){
             queryUser.get("A6sgIWJZyD", {
                 success: function(user) {
                     var roleACL = new Parse.ACL();
-                    var role = new Parse.Role("Leitor", roleACL);
+                    var role = new Parse.Role("LeitorX", roleACL);
                     role.getUsers().add(user);
                     role.getRoles().add(Parse.Role("Leitor", roleACL));
                     role.save();
