e5f80430ae3a9c1984b3e7dd8d8d471bc36dc6e0
Rafael Augusto da Silva Ribeiro <rafael@exla.com.br>
2014-10-08 09:10:22 -0300

    Testando

--- js/init.js
+++ js/init.js
@@ -40,7 +40,7 @@ function adicionarRole(){
 
 function adicionarUserInRoles(){
 
-    var User = Parse.Object.extend("_User");
+    var User = Parse.Object.extend("User");
     var Role = Parse.Object.extend("_Role");
 
     var queryUser = new Parse.Query(User);
